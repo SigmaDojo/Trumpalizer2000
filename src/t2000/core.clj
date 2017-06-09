e86b2a129bca064227e90c0ced68b553d5b8bbdf
Anders Nilsson <anders.nilsson@sigma.se>
2017-06-09 23:37:11 +0200

    Return sorted data

--- src/t2000/core.clj
+++ src/t2000/core.clj
@@ -16,37 +16,17 @@
 (defn tweets->date
   "Extract create-date from each tweet"
   [tweets]
-  (map
-   (fn [tweet] (twit/parse-twitter-date (:created_at tweet)))
-   tweets))
+  (map #(twit/parse-twitter-date (:created_at %)) tweets))
 
 
-;; TODO: generate this?
+
 ;; (def empty-freq  (zipmap (map #(format "%02d" %) (range 24)) (repeat 0)))
-(def empty-freq {"00" 0
-                 "01" 0
-                 "02" 0
-                 "03" 0
-                 "04" 0
-                 "05" 0
-                 "06" 0
-                 "07" 0
-                 "08" 0
-                 "09" 0
-                 "10" 0
-                 "11" 0
-                 "12" 0
-                 "13" 0
-                 "14" 0
-                 "15" 0
-                 "16" 0
-                 "17" 0
-                 "18" 0
-                 "19" 0
-                 "20" 0
-                 "21" 0
-                 "22" 0
-                 "23" 0})
+(def empty-freq {"00" 0  "01" 0  "02" 0  "03" 0
+                 "04" 0  "05" 0  "06" 0  "07" 0
+                 "08" 0  "09" 0  "10" 0  "11" 0
+                 "12" 0  "13" 0  "14" 0  "15" 0
+                 "16" 0  "17" 0  "18" 0  "19" 0
+                 "20" 0  "21" 0  "22" 0  "23" 0})
 
 
 (defn extract-hour [date]
@@ -54,9 +34,9 @@
 
 
 (defn group-tweets [tweets]
-  (let [hours (map extract-hour (tweets->date tweets))]
-    (merge empty-freq
-           (frequencies hours))))
+  (into (sorted-map)
+        (merge empty-freq
+               (frequencies (map extract-hour (tweets->date tweets))))))
 
 
 (defn tweet-freq->coords [tweets]
